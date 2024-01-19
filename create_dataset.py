a221000d37f5a8b1c72535461b053731426184a7
Sander Venema <s.venema.6@student.rug.nl>
2024-01-19 11:27:20 +0100

    extend dataset

--- create_dataset.py
+++ create_dataset.py
@@ -1,54 +1,44 @@
 import os
 import shutil
+from tqdm import tqdm
 
 def list_directories_starting_with(directory, prefix):
     directories = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name)) and name.startswith(prefix)]
     return directories
 
-# Replace 'your_directory_path' with the actual path to the directory you want to search in
-directory_path = 'data/seg3D/train/images/'
-prefix_to_search = 'BreaDM-Ma-'
+ttv = ['train', 'test', 'val']
+types = ['images', 'labels']
+prefixes = ['BreaDM-Ma-', 'BreaDM-Be-']
 
-file_list = list_directories_starting_with(directory_path, prefix_to_search)
+directory_path = 'data/seg/'
 
 img_dir = 'data/seg/train/images/'
 
-# Create a new directory to store the images
-new_directory = 'new_dataset/images/'
+new_directory = 'new_dataset/'
 os.makedirs(new_directory, exist_ok=True)
 
-for folder_name in file_list:
-    source_path = os.path.join(img_dir, folder_name)
-    destination_path = os.path.join(source_path, 'VIBRANT')
-    print(destination_path)
+for split in ttv:
+    print('Split: ' + split)
+    for soort in types:
+        print('Type: ' + soort)
+        for prefix in prefixes:
+            print('Prefix: ' + prefix)
+            directory_path = 'data/seg/' + split + '/' + soort + '/'
+            file_list = list_directories_starting_with(directory_path, prefix)
+            img_dir = 'data/seg/' + split + '/' + soort + '/'
 
-    # Copy the contents of the source folder to the 'VIBRANT' folder in the new directory and add a prefix to the file names
-    for filename in os.listdir(destination_path):
-        img = os.path.join(destination_path, filename)
-        print(img)
-        print(new_directory)
-        shutil.copy(img, new_directory)
-        os.rename(os.path.join(new_directory, filename), os.path.join(new_directory, folder_name + '_' + filename))
+            for folder_name in tqdm(file_list):
+                source_path = os.path.join(img_dir, folder_name)
+                destination_path = os.path.join(source_path, 'VIBRANT')
 
-directory_path = 'data/seg/train/labels/'
-prefix_to_search = 'BreaDM-Ma-'
-
-file_list = list_directories_starting_with(directory_path, prefix_to_search)
-
-img_dir = 'data/seg/train/labels/'
-
-# Create a new directory to store the images
-new_directory = 'new_dataset/labels/'
-os.makedirs(new_directory, exist_ok=True)
-
-for folder_name in file_list:
-    source_path = os.path.join(img_dir, folder_name)
-    destination_path = os.path.join(source_path, 'VIBRANT')
-    print(destination_path)
-
-    for filename in os.listdir(destination_path):
-        img = os.path.join(destination_path, filename)
-        print(img)
-        print(new_directory)
-        shutil.copy(img, new_directory)
-        os.rename(os.path.join(new_directory, filename), os.path.join(new_directory, folder_name + '_' + filename))
+                # Copy the contents of the source folder to the 'VIBRANT' folder in the new directory and add a prefix to the file names
+                for filename in os.listdir(destination_path):
+                    img = os.path.join(destination_path, filename)
+                    if prefix == 'BreaDM-Ma-':
+                        fin_path = new_directory + soort + '/' + 'malignant' + '/'
+                    else:
+                        fin_path = new_directory + soort + '/' + 'benign' + '/'
+                    
+                    os.makedirs(fin_path, exist_ok=True)
+                    shutil.copy(img, fin_path)
+                    os.rename(os.path.join(fin_path, filename), os.path.join(fin_path, folder_name + '_' + filename))
