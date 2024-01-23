8c248ac28fe331b291a4cd6805e83cc5172baf19
Sander Venema <s.venema.6@student.rug.nl>
2024-01-23 13:29:51 +0100

    focal to binary

--- utils/losses.py
+++ utils/losses.py
@@ -34,16 +34,14 @@ class BCEDiceLoss(nn.Module):
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=0, size_average=True, ignore_index=255):
+    def __init__(self, alpha=2, gamma=0.25, size_average=True):
         super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
-        self.ignore_index = ignore_index
         self.size_average = size_average
 
     def forward(self, inputs, targets):
-        ce_loss = F.cross_entropy(
-            inputs, targets, reduction='none', ignore_index=self.ignore_index)
+        ce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
         pt = torch.exp(-ce_loss)
         focal_loss = self.alpha * (1-pt)**self.gamma * ce_loss
         if self.size_average:
