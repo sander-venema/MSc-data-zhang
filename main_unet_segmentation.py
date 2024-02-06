5e1414e4cf25544a143fd41404a5b4d44ef979fb
Sander Venema S.Venema.6@student.rug.nl <s.venema.6@student.rug.nl>
2024-02-06 15:29:50 +0100

    6/2

--- main_unet_segmentation.py
+++ main_unet_segmentation.py
@@ -42,7 +42,7 @@ train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, dr
 val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
 
 model = Unet(
-    backbone='vgg16_bn',
+    backbone='resnext101_32x8d',
     in_channels=3,
     num_classes=1,
 )
@@ -56,7 +56,7 @@ optimizer = optim.AdamW(params, lr=LEARNING_RATE)
 model.to(device)
 
 loss_short = 'bce_dice' if args.loss == 0 else 'iou' if args.loss == 1 else 'dice' if args.loss == 2 else 'lovasz' if args.loss == 3 else 'bce_xloss' if args.loss == 4 else 'focal' if args.loss == 5 else 'bce' if args.loss == 6 else 'dice_bce'
-run_name = "unet_vgg16bn_{0}_{1}_new".format(loss_short, LEARNING_RATE)
+run_name = "unet_resnext101_32_{0}_{1}_new".format(loss_short, LEARNING_RATE)
 
 writer = SummaryWriter(f"logs_segmentation/{run_name}")
 
