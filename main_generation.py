79a1e7e4aadd4b7b71c3ee42eb00d84aeef5e82a
Sander Venema S.Venema.6@student.rug.nl <s.venema.6@student.rug.nl>
2024-03-13 15:25:58 +0100

    13-03

--- main_generation.py
+++ main_generation.py
@@ -15,6 +15,7 @@ parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
 parser.add_argument('--learning_rate', type=float, default=5e-5, help='Learning rate')
 parser.add_argument('--latent_dim', type=int, default=100, help='Latent dimension')
 parser.add_argument('--num_epochs', type=int, default=1000, help='Number of epochs')
+parser.add_argument('--lamb', type=int, default=0.1, help='Lambda value for gradient penalty')
 
 args = parser.parse_args()
 
@@ -22,6 +23,7 @@ BATCH_SIZE = args.batch_size
 LEARNING_RATE = args.learning_rate
 latent_dim = args.latent_dim
 num_epochs = args.num_epochs
+LAMBDA = args.lamb
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -29,10 +31,39 @@ transform = transforms.Compose([
     transforms.Normalize((0.5,), (0.5,))
 ])
 
+def calc_gradient_penalty(netD, real_data, fake_data, batch_size, dim, device, gp_lambda):
+    alpha = torch.rand(batch_size, 1)
+    alpha = alpha.expand(batch_size, int(real_data.nelement()/batch_size)).contiguous()
+    alpha = alpha.view(batch_size, 1, dim, dim)
+    alpha = alpha.to(device)
+    
+    fake_data = fake_data.view(batch_size, 1, dim, dim)
+    interpolates = alpha * real_data.detach() + ((1 - alpha) * fake_data.detach())
+
+    interpolates = interpolates.to(device)
+    interpolates.requires_grad_(True)
+
+    disc_interpolates = netD(interpolates)
+
+    gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+                              grad_outputs=torch.ones(disc_interpolates.size()).to(device),
+                              create_graph=True, retain_graph=True, only_inputs=True)[0]
+
+    gradients = gradients.view(gradients.size(0), -1)                              
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * gp_lambda
+    return gradient_penalty
+
+wasserstein_loss = lambda y_true, y_pred: -torch.mean(y_true * y_pred)
+
 dataset = GenerationDataset(root_dir="new_dataset/train/images/", transform=transform)
 
 # Use KFold for 3-fold cross-validation
-kf = KFold(n_splits=3, shuffle=True, random_state=42)
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+one = torch.tensor(1, dtype=torch.float)
+mone = one * -1
+one.to("cuda")
+mone.to("cuda")
 
 for fold, (train_index, val_index) in enumerate(kf.split(dataset)):
     G = Generator_3().to("cuda")
@@ -50,7 +81,7 @@ for fold, (train_index, val_index) in enumerate(kf.split(dataset)):
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
     val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
-    filename = "wass_{0}_fold_{1}".format(LEARNING_RATE, fold)
+    filename = "wassGP_{0}_5fold_{1}".format(LEARNING_RATE, fold)
     saving_path = "generated_images/{0}".format(filename)
     writer = SummaryWriter(f"logs_generation/{filename}")
     os.makedirs(saving_path, exist_ok=True)
@@ -82,16 +113,23 @@ for fold, (train_index, val_index) in enumerate(kf.split(dataset)):
             # Real images
             real_outputs = D(real_imgs)
             real_score += real_outputs.mean().item()
+            d_loss_real = real_outputs.mean()
 
-            print("realmean: {}".format(real_outputs.mean()))
             # Fake images
             z = torch.randn(batch_size, latent_dim).to("cuda")
             fake_imgs = G(z)
             fake_outputs = D(fake_imgs.detach())
             fake_score += fake_outputs.mean().item()
-            print("fakemean: {}".format(fake_outputs.mean()))
+            d_loss_fake = fake_outputs.mean()
 
-            d_loss = -torch.mean(real_outputs) + torch.mean(fake_outputs)
+            # real_loss = wasserstein_loss(D(real_imgs), -torch.ones_like(D(real_imgs)))
+            # fake_loss = wasserstein_loss(D(fake_imgs), torch.ones_like(D(fake_imgs)))
+            # d_loss = real_loss + fake_loss
+
+            gradient_penalty = calc_gradient_penalty(D, real_imgs.data, fake_imgs.data, batch_size, 369, "cuda", LAMBDA)
+
+            # d_loss = -torch.mean(real_outputs) + torch.mean(fake_outputs)
+            d_loss = d_loss_fake - d_loss_real + gradient_penalty
             d_loss.backward()
             optimizer_D.step()
 
@@ -101,16 +139,16 @@ for fold, (train_index, val_index) in enumerate(kf.split(dataset)):
 
             # Discriminator accuracy
             total_real += batch_size
-            correct_real += (real_outputs > 0.5).sum().item()
+            correct_real += (torch.sigmoid(real_outputs) >= 0.5).sum().item()
 
             total_fake += batch_size
-            correct_fake += (fake_outputs <= 0.5).sum().item()
+            correct_fake += (torch.sigmoid(fake_outputs) < 0.5).sum().item()
 
             # ---------------------
             #  Train Generator
             # ---------------------
 
-            if (i + 1) % 5 == 0 and i >= 9: # Train the generator every 5 batches
+            if (i + 1) % 5 == 0: # Train the generator every 5 batches
                 G.zero_grad()
 
                 # Generate fake images
@@ -119,9 +157,11 @@ for fold, (train_index, val_index) in enumerate(kf.split(dataset)):
                 fake_outputs = D(fake_imgs)
 
                 # Generator loss
-                g_loss = -torch.mean(D(fake_imgs))
-                g_loss.backward()
+                # g_loss = wasserstein_loss(fake_outputs, -torch.ones_like(fake_outputs))
+                g_loss = torch.mean(D(fake_imgs))
+                g_loss.backward(mone)
                 optimizer_G.step()
+                g_loss = -g_loss
 
         accuracy_real = correct_real / total_real
         accuracy_fake = correct_fake / total_fake
@@ -157,8 +197,8 @@ for fold, (train_index, val_index) in enumerate(kf.split(dataset)):
             # Calculate discriminator loss and accuracy
             d_loss = torch.mean(D(fake_imgs)) - torch.mean(D(val_imgs))
             val_loss += d_loss.item()
-            val_acc_real += (real_outputs > 0.5).sum().item()
-            val_acc_fake += (fake_outputs <= 0.5).sum().item()
+            val_acc_real += (torch.sigmoid(real_outputs) >= 0.5).sum().item()
+            val_acc_fake += (torch.sigmoid(fake_outputs) < 0.5).sum().item()
 
             num_val_batches += 1
 
@@ -173,8 +213,9 @@ for fold, (train_index, val_index) in enumerate(kf.split(dataset)):
         writer.add_scalar('Validation/Accuracy/Fake', val_acc_fake, epoch)
 
         # Save the best model
-        if val_acc_real > 0.8 and val_acc_real >= cur_best_real and val_acc_fake > 0.8 and val_acc_fake >= cur_best_fake:
-            print(f"Saving new best model at epoch {epoch}, fold {fold}, with real accuracy {val_acc_real} and fake accuracy {val_acc_fake}")
+        if val_acc_real > 0.8 and val_acc_real >= cur_best_real and val_acc_fake > 0.8 and val_acc_fake >= cur_best_fake and epoch > 50:
+            with open(f"saved_models/{filename}_best.txt", "a+") as f:
+                f.write(f"Epoch: {epoch}, Real accuracy: {val_acc_real}, Fake accuracy: {val_acc_fake}\n")
             cur_best_real = val_acc_real
             cur_best_fake = val_acc_fake
             torch.save({
