01fd515176aa7237b5b23a60b9c8073199b073a2
Sander Venema S.Venema.6@student.rug.nl <s.venema.6@student.rug.nl>
2024-04-02 15:43:35 +0200

    2/4

--- augment_dataset.py
+++ augment_dataset.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 import numpy as np
+import shutil
+from tqdm import tqdm
 
 # Function to perform horizontal flip augmentation
 def horizontal_flip(image):
@@ -8,7 +10,7 @@ def horizontal_flip(image):
 
 # Define paths
 base_path = 'new_dataset'
-splits = ['train', 'test']
+splits = ['train/', 'test/']
 
 # Create directory for augmented dataset
 output_base_path = 'augmented_dataset'
@@ -16,20 +18,20 @@ os.makedirs(output_base_path, exist_ok=True)
 
 # Iterate through train and test splits
 for split in splits:
-    images_path = os.path.join(base_path, split, 'images')
-    masks_path = os.path.join(base_path, split, 'labels')
-    save_images_path = os.path.join(output_base_path, split, 'images')
-    save_masks_path = os.path.join(output_base_path, split, 'labels')
+    images_path = os.path.join(base_path, split + 'images/')
+    # masks_path = os.path.join(base_path, split, 'labels')
+    save_images_path = os.path.join(output_base_path, split + 'images/')
+    # save_masks_path = os.path.join(output_base_path, split, 'labels')
 
     # Create directories for saving augmented images
     os.makedirs(save_images_path, exist_ok=True)
-    os.makedirs(save_masks_path, exist_ok=True)
+    # os.makedirs(save_masks_path, exist_ok=True)
 
     # List images in the current split
     image_files = os.listdir(images_path)
 
     # Perform augmentation for each image
-    for image_file in image_files:
+    for image_file in tqdm(image_files):
         # Load image and mask
         image = cv2.imread(os.path.join(images_path, image_file))
         # mask = cv2.imread(os.path.join(masks_path, image_file))
@@ -40,6 +42,9 @@ for split in splits:
 
         # Save augmented images
         cv2.imwrite(os.path.join(save_images_path, image_file), augmented_image)
+        os.rename(os.path.join(save_images_path, image_file), os.path.join(save_images_path, image_file.split('_')[0] + '_flip_' + image_file.split('_')[1]))
         # cv2.imwrite(os.path.join(save_masks_path, image_file), augmented_mask)
 
+    shutil.copytree(images_path, save_images_path, dirs_exist_ok=True)
+    
 print("Augmentation completed successfully!")
--- main_generation.py
+++ main_generation.py
@@ -17,6 +17,7 @@ parser.add_argument('--latent_dim', type=int, default=100, help='Latent dimensio
 parser.add_argument('--num_epochs', type=int, default=1000, help='Number of epochs')
 parser.add_argument('--lamb', type=int, default=0.1, help='Lambda value for gradient penalty')
 parser.add_argument('--complete_dataset', action='store_true', default=False, help='Whether to use the complete dataset')
+parser.add_argument('--augmented_dataset', action='store_true', default=False, help='Whether to use the augmented dataset')
 
 args = parser.parse_args()
 
@@ -58,8 +59,13 @@ wasserstein_loss = lambda y_true, y_pred: -torch.mean(y_true * y_pred)
 
 if args.complete_dataset:
     dataset = GenerationDataset(root_dir="complete_dataset/train/images/", transform=transform)
+    print("Using complete dataset")
+elif args.augmented_dataset:
+    dataset = GenerationDataset(root_dir="augmented_dataset/train/images/", transform=transform)
+    print("Using augmented dataset")
 else:
     dataset = GenerationDataset(root_dir="new_dataset/train/images/", transform=transform)
+    print("Using original dataset")
 
 # Use KFold for 3-fold cross-validation
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
@@ -85,7 +91,7 @@ for fold, (train_index, val_index) in enumerate(kf.split(dataset)):
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
     val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
-    filename = "wasslargevib{0}_{1}_5fold_{2}".format(BATCH_SIZE, LEARNING_RATE, fold)
+    filename = "wassaug{0}_{1}_5fold_{2}".format(BATCH_SIZE, LEARNING_RATE, fold)
     saving_path = "generated_images/{0}".format(filename)
     writer = SummaryWriter(f"logs_generation/{filename}")
     os.makedirs(saving_path, exist_ok=True)
